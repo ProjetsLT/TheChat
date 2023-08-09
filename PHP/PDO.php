5d0c334bb08be4d4ed908ceb2bb4c65bb555a4cd
ProjetLT <lilian.taillandier@proton.me>
2023-08-09 14:29:55 +0200

    connexion à la bdd

---
+++ PHP/PDO.php
@@ -0,0 +1,18 @@
+<?php
+
+$addServeur = '127.0.0.1';
+$nomBdd = 'TheChat';
+$utilisateur = 'root';
+$mdpBdd = '';
+
+try {
+    $bdd = new PDO("mysql:host=$addServeur;dbname=$nomBdd", $utilisateur, $mdpBdd); // connexion à la BDD (méthode PDO pour "multi-plateforme")
+
+    // Configure le mode d'erreur PDO sur Exception
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch (PDOException $e) {
+    echo "Erreur de connexion à la base de données: " . $e->getMessage();
+}
+
+?>
