17f0b09ea770b3eca82ac5e50342ab464441a705
ProjetLT <lilian.taillandier@proton.me>
2023-08-22 15:28:46 +0200

    mise en page de la page erreurs.php

--- PHP/erreurs.php
+++ PHP/erreurs.php
@@ -10,18 +10,34 @@
     integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Page d'Erreur</title>
 </head>
-<body class="flex flex-col items-center justify-center h-screen bg-gray-100">
+<body class="bg-[url('../images/Cloudy.svg')] bg-cover flex flex-col items-center justify-center h-screen">
 
-    <div class="flex flex-col items-center">
-        <div class="w-1/2 mt-2">
-            <lottie-player src="../images/animationErreur.json" autoplay loop class="h-60"></lottie-player>
+    <div class="flex flex-col items-center bg-gradient-to-r from-indigo-300 to-teal-300 rounded-lg shadow-md p-8 mt-4">
+
+        <div class="order-1 sm:order-1 text-center">
+            <h1 class="text-red-500 text-3xl font-semibold mb-3">Erreur</h1>
+            <p class="text-gray-700 text-lg"><?php echo isset($_GET['message']) ? htmlspecialchars($_GET['message']) : "Une erreur inconnue s'est produite."; ?></p>
         </div>
 
-        <div class="bg-white rounded-lg shadow-md p-8 mt-4">
-            <h1 class="text-red-500 text-2xl font-semibold mb-2 text-center">Erreur</h1>
-            <p class="text-gray-700"><?php echo isset($_GET['message']) ? htmlspecialchars($_GET['message']) : "Une erreur inconnue s'est produite."; ?></p>
+        <div class="w-1/2 mt-2 order-2 sm:order-2">
+            <lottie-player src="../images/animationErreur.json" autoplay loop class="h-70"></lottie-player>
         </div>
+
+        <div class="mt-2 order-3 sm:order-3">
+            <button onclick="goBack()" type="submit" name="btnRetour" name="btnRetour"
+            class="border-2 border-indigo-700 bg-indigo-700 
+            text-white py-1 px-4 w-full rounded-lg flex items-center justify-center gap-2 hover:bg-transparent hover:text-indigo-700 
+            font-semibold text-lg"> Retour <i class="fa-solid fa-rotate-left"></i>
+            </button>
+        </div>
+
     </div>
+
+    <script>
+        function goBack() {
+            window.history.back();
+        }
+    </script>
     
 </body>
 </html>
