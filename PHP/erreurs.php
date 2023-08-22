25bfb5d3bf6ac509dabac3fe8dbff93088bb3fa7
ProjetLT <lilian.taillandier@proton.me>
2023-08-22 12:35:07 +0200

    ajout du style de la page d'erreur

--- PHP/erreurs.php
+++ PHP/erreurs.php
@@ -3,35 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@0.4.0/dist/lottie-player.js"></script>
+    <link rel="shortcut icon" href="../images/Logo_TheChat_sans_écriture.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" 
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Page d'Erreur</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f8f8f8;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-        .error-container {
-            background-color: #ffffff;
-            border-radius: 8px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-            padding: 20px;
-            text-align: center;
-        }
-        h1 {
-            margin-top: 0;
-            color: #e74c3c;
-        }
-    </style>
 </head>
-<body>
-    <div class="error-container">
-        <h1>Erreur</h1>
-        <p><?php echo isset($_GET['message']) ? htmlspecialchars($_GET['message']) : "Une erreur inconnue s'est produite."; ?></p>
+<body class="flex flex-col items-center justify-center h-screen bg-gray-100">
+
+    <div class="flex flex-col items-center">
+        <div class="w-1/2 mt-2">
+            <lottie-player src="../images/animationErreur.json" autoplay loop class="h-60"></lottie-player>
+        </div>
+
+        <div class="bg-white rounded-lg shadow-md p-8 mt-4">
+            <h1 class="text-red-500 text-2xl font-semibold mb-2 text-center">Erreur</h1>
+            <p class="text-gray-700"><?php echo isset($_GET['message']) ? htmlspecialchars($_GET['message']) : "Une erreur inconnue s'est produite."; ?></p>
+        </div>
     </div>
+    
 </body>
 </html>
