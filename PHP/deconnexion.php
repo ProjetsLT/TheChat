2513a22f7fd5eb99c50c2c003bbbc44600a71b68
ProjetLT <lilian.taillandier@proton.me>
2023-08-23 15:28:22 +0200

    ajout de la fonctionnalité de deconnexion

--- PHP/deconnexion.php
+++ PHP/deconnexion.php
@@ -1,3 +1,10 @@
 <?php
+session_start(); // on démarre les sessions
+
+$_SESSION = array(); // on récupère les sessions active dans un tableau
+
+session_destroy(); // on détruit la sessions en cours de l'utilisateur
+
+header("Location: ../index.html"); // une fois que tout est éffectuer on redirige vers la page d'accueil
 
 ?>
