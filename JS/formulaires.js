892f12a962d3259264870b8f8fb821e8dd765b1c
ProjetLT <lilian.taillandier@proton.me>
2023-08-25 17:23:46 +0200

    rajout de fonctionnalités réactive en js

--- JS/formulaires.js
+++ JS/formulaires.js
@@ -29,3 +29,27 @@ document.addEventListener("DOMContentLoaded", function () {
     passwordLengthMessage.classList.add("text-red-600");
     passwordSpecialCharMessage.classList.add("text-red-600");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordInput = document.getElementById("mdpIns");
+    const confirmPasswordInput = document.getElementById("mdpIns2");
+    const passwordMatchMessage = document.getElementById("passwordMatchMessage");
+
+    confirmPasswordInput.addEventListener("input", function () {
+        const password = passwordInput.value;
+        const confirmPassword = confirmPasswordInput.value;
+
+        if (password != confirmPassword) {
+            passwordMatchMessage.textContent = "Les mots de passe ne correspondent pas";
+            passwordMatchMessage.classList.add("text-red-600");
+            passwordMatchMessage.classList.remove("text-green-700");
+        } else {
+            passwordMatchMessage.textContent = "Les mots de passe correspondent";
+            passwordMatchMessage.classList.remove("text-red-600");
+            passwordMatchMessage.classList.add("text-green-700");
+        }
+    });
+
+    // Initial setup to show message in red
+    passwordMatchMessage.classList.add("text-red-600");
+});
