7ef0e0d09d55f945507c75b35c46e2c0d6294153
ProjetLT <lilian.taillandier@proton.me>
2023-08-25 21:53:56 +0200

    ajouts de sécurité pour le mot de passe à l'inscription

--- PHP/inscription.php
+++ PHP/inscription.php
@@ -12,41 +12,69 @@ if(isset($_POST['btnIns']))
     $mdp2 = sha1($_POST['mdpIns2']);
     $pseudo = $_POST['pseudo'];
 
-    if(!empty($_POST['nom']) AND !empty($_POST['prenom']) AND !empty($_POST['mail']) AND !empty($_POST['pseudo']) 
-    AND !empty($_POST['mdpIns']) AND !empty($_POST['mdpIns2']))
+    if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['mail']) && !empty($_POST['pseudo']) 
+    && !empty($_POST['mdpIns']) && !empty($_POST['mdpIns2']))
     {
         if(filter_var($mail, FILTER_VALIDATE_EMAIL))
         {
             $reqMail = $bdd->prepare("SELECT * FROM membres WHERE mail = ?"); 
             $reqMail->execute(array($mail));
-            $mailExist = $reqMail->rowcount();
+            $mailExist = $reqMail->rowCount();
             if($mailExist == 0)  
             {
                 if($mdp == $mdp2)
                 {
-                    $insertMbr = $bdd->prepare("INSERT INTO membres(nom, prenom, mail, pseudo, mdp) VALUES(?,?,?,?,?)");
-                    $insertMbr->execute(array($nom, $prenom, $mail, $pseudo, $mdp));
-                    // $_SESSION['pseudo'] = $pseudo; // si besoin d'aller direct page profil
-                    // header('Location: profil.php');  
-                    header('Location: ../Formulaires/connexion.html');  
-                    exit;
-                } else {
-                    $message = "Les mots de passe de correspondent pas";
+                    // Vérification de la longueur minimale du mot de passe
+                    if(strlen($_POST['mdpIns']) >= 8)
+                    {
+                        // Vérification de la présence d'au moins un caractère spécial
+                        if(preg_match('/[!@#$%^&*(),.?":{}|<>]/', $_POST['mdpIns']))
+                        {
+                            $insertMbr = $bdd->prepare("INSERT INTO membres(nom, prenom, mail, pseudo, mdp) VALUES(?,?,?,?,?)");
+                            $insertMbr->execute(array($nom, $prenom, $mail, $pseudo, $mdp));
+                            // $_SESSION['pseudo'] = $pseudo; // si besoin d'aller direct page profil
+                            // header('Location: profil.php');  
+                            header('Location: ../Formulaires/connexion.html');  
+                            exit;
+                        }
+                        else
+                        {
+                            $message = "Le mot de passe doit contenir au moins un caractère spécial";
+                            header('Location: erreurs.php?message=' . urlencode($message));
+                            exit;
+                        }
+                    }
+                    else
+                    {
+                        $message = "Le mot de passe doit avoir au moins 8 caractères";
+                        header('Location: erreurs.php?message=' . urlencode($message));
+                        exit;
+                    }
+                } 
+                else 
+                {
+                    $message = "Les mots de passe ne correspondent pas";
                     header('Location: erreurs.php?message=' . urlencode($message));
                     exit;
                 }
-            } else {
-                $message = "Le mail existe déjà";
+            } 
+            else 
+            {
+                $message = "L'adresse email existe déjà";
                 header('Location: erreurs.php?message=' . urlencode($message));
                 exit;
             }
-        } else {
-            $message = "email non valide";
+        } 
+        else 
+        {
+            $message = "Adresse email non valide";
             header('Location: erreurs.php?message=' . urlencode($message));
             exit;
         }
-    } else {
-        $message = "Tout les champs doivent-être compléter";
+    } 
+    else 
+    {
+        $message = "Tous les champs doivent être complétés";
         header('Location: erreurs.php?message=' . urlencode($message));
         exit;
     }
